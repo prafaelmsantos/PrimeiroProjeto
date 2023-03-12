d13f1f5c25dbd8df6729c11fa0040c8cd224c0bb
Rafael Santos <prafaelmsantos@gmail.com>
2023-03-12 23:34:35 +0000

    12/03/2023

---
+++ PrimeiroProjeto/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PrimeiroProjeto.Models;
+using System.Diagnostics;
+
+namespace PrimeiroProjeto.Controllers
+{
+    /* Os metodos desta classe serve para processar requisições,
+        onde cada um ddestes metodos equivale a um endpoint ( actions) */
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ViewResult Index(int? id)
+        {
+            return View(id);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
